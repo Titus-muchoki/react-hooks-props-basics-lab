dad5d02311114c2691851fa28e4edf41b167303e
kajela <titoyut56@gmail.com>
2023-07-19 09:18:04 +0300

    add the initial files

--- src/components/About.js
+++ src/components/About.js
@@ -1,12 +1,16 @@
 import React from "react";
+import Links from "./Links";
 
-function About() {
+function About(props) {
   return (
     <div id="about">
       <h2>About Me</h2>
-      <p>Put the bio in here</p>
+      <p>I made this</p>
       <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
-      {/* add your <Links /> component here */}
+      {/* <h1>Links</h1> */}
+      <Links github={props.links.github} linkedin={props.links.linkedin}/>
+      {/* {{ links: props.}} */}
+     {/* {props.bio} {props.links} */}
     </div>
   );
 }
